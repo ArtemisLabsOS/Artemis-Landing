4d43ea265519055fa3b93dd34188f96ca9da02db
SeongChoi <choies921003@gmail.com>
2019-12-19 18:30:50 -0800

    edited structure

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -5,38 +5,40 @@ const Intro: React.FC = () => {
   return (
     <div>
       <div>
-      <img src="" height='100px' />
-      <h1></h1>
-      <p id='artemis-bio'>
-      Project Artemis is an Open Sourced client developer tool for single endpoint testing of GraphQL queries with Apollo Client. 
-      Query endpoint testing and cache management can be done in real time for instant engineering feedback without the need for backend resolvers.
-      </p>
-      <p>
-      Debug and test query mutations and calls using introspection
-      The web based extension is best used for React and Express products/applications in development mode, and is available at the chrome store. 
-        
-      Core features:
-    <ul>
-           <li>1. Display schema, query, and GraphQL response with endpoint detection </li> 
-           <li> 2. Query caching and component storage- View normalized Apollo Client cache in JSON Format</li>
-           <li>3. History of past queries per testing session </li> 
-          <li>4. Query response Snapshot featured</li>
-          <li>5. View Response and Query Visualizer tree in inspection window</li>
-           <li> 6. Performance monitoring and Management </li>
-   </ul>
-       </p>
-      <p>See your GraphQL schema, queries, and cache in real-time</p>
-      <p> Includes query snapshot </p>
-      <video loop autoPlay controls width='100%' muted >
-        <source src='https://twitter.com/i/status/1207729912892678145' type='video/mp4' />
-      </video>
-      <p> To utilize, open the inspector window. If a website or hosted site you are currently testing contains Apollo Client. 
+        <img src="" height='100px' />
+        <div id='artemis-bio'>
+          <h1>Artemis Bio</h1>
+          <p>
+            Project Artemis is an Open Sourced client developer tool for single endpoint testing of GraphQL queries with Apollo Client.
+            Query endpoint testing and cache management can be done in real time for instant engineering feedback without the need for backend resolvers.
+          </p>
+        </div>
+        <div>
+          {/* <p>
+        Debug and test query mutations and calls using introspection.
+        The web based extension is best used for React and Express products/applications in development mode, and is available at the chrome store.    
+        </p> */}
+          <h3>Core features:</h3>
+          <ul>
+            <li>1. Display schema, query, and GraphQL response with endpoint detection </li>
+            <li>2. Query caching and component storage- View normalized Apollo Client cache in JSON Format</li>
+            <li>3. History of past queries per testing session </li>
+            <li>4. Query response Snapshot featured</li>
+            <li>5. View Response and Query Visualizer tree in inspection window</li>
+            <li>6. Performance monitoring and Management </li>
+          </ul>
+        </div>
+
+        {/* <p>See your GraphQL schema, queries, and cache in real-time</p>
+        <p> Includes query snapshot </p> */}
+        {/* */}
+        <p> To utilize, open the inspector window. If a website or hosted site you are currently testing contains Apollo Client.
         Artemis will appear and will render once a GraphQL query is detected. </p>
       </div>
       <h3>Like our developer tool? Give us a star!</h3>
 
       <div id='github-btns'>
-          <a
+        <a
           id='github-watch'
           className='github-button'
           href='https://github.com/ArtemisLabsLLP/Artemis-dev-tool'
--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,44 +1,35 @@
-import React , { Fragment} from "react";
-import { MuiThemeProvider,Tab, CssBaseline, Menu, Tabs, Link } from "@material-ui/core";
+import * as React from "react";
+import { MuiThemeProvider, Tab } from "@material-ui/core";
 import useDarkMode from "use-dark-mode";
 import { lightTheme, darkTheme } from "../themes/theme";
-import { BrowserRouter as Router, Route, BrowserRouter } from "react-router-dom";
-import { Switch, withRouter } from 'react-router-dom';
-import SearchBar from 'material-ui-search-bar'
 
-            //value={this.state.value}
-            // onChange={(newValue) => this.setState({ value: newValue })}
-            // onRequestSearch={() => doSomethingWith(this.state.value)}
-
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
+//value={this.state.value}
+// onChange={(newValue) => this.setState({ value: newValue })}
+// onRequestSearch={() => doSomethingWith(this.state.value)}
 
 import "../Styles/styles.css"
 // <div id="searchBar">
 //{/* <SearchBar
-          
+
 ///></div>   */}
-const Header: React.FC = () => {  
+const Header: React.FC = () => {
   const darkMode = useDarkMode();
   const theme = darkMode.value ? darkTheme : darkTheme;
 
-//<a href={../components/projectArtemis.tsx}></a>
+  //<a href={../components/projectArtemis.tsx}></a>
   return (
     <MuiThemeProvider theme={theme}>
-        <span id="header">
-                
-                  <Tab label="Home"/>  
-                  <Tab  label="Download" />
-                  <Tab  label="Demo" />
-                  <a href="https://github.com/ArtemisLabsLLP/Artemis-dev-tool"><Tab label="Contribute" /></a>
-                  <a href="https://www.patreon.com/artemislabs"> <Tab label="Donate" /> </a>
-                  <a href="https://medium.com/artemis-labs-open-source"> <Tab label="Media"/> </a>
-                 
-       </span>
-      </MuiThemeProvider>
+      <span id="header">
+
+        <Tab label="Home" />
+        <Tab label="Download" />
+        <Tab label="Demo" />
+        <a href="https://github.com/ArtemisLabsLLP/Artemis-dev-tool"><Tab label="Contribute" /></a>
+        <a href="https://www.patreon.com/artemislabs"> <Tab label="Donate" /> </a>
+        <a href="https://medium.com/artemis-labs-open-source"> <Tab label="Media" /> </a>
+
+      </span>
+    </MuiThemeProvider>
   );
 };
 
